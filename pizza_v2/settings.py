7f774ef3c86db04702315c2443c6f2702cead982
danekpr1 <112620766+danekpr1@users.noreply.github.com>
2023-05-04 13:48:24 +0000

    tengja server við settings py

--- pizza_v2/settings.py
+++ pizza_v2/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': 'pizzalair-db-user',
         'PASSWORD': ')GZKvIo%gAXdu%Si',
         'HOST':'34.88.126.226',
-        'PORT':'5432'
+        'PORT':'5432',
     }
 }
 
