c9858c4966cd9d43a2edef41eadf2561f2464e9a
dorajohanns <89972552+dorajohanns@users.noreply.github.com>
2023-05-05 13:35:23 +0000

    detail page fyrir menu

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-urlpatterns = [
 
+urlpatterns = [
     path('', views.index, name="menu"),
+    path('<int:id>', views.get_pizza_by_id, name="pizza-details")
 ]
