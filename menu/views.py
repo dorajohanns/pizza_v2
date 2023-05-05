128ea929810946f47d9461f6b2516c84297d406c
danekpr1 <112620766+danekpr1@users.noreply.github.com>
2023-05-05 14:10:10 +0000

    login og logout, og alskonar

--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from menu.models import Pizza
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        pizzas = [{
+            'id':x.id,
+            'name':x.name,
+            'toppings':x.toppings,
+            'image':x.image,
+        }for x in Pizza.objects.filter(name__icontains=search_filter)]
+
+        return JsonResponse({'data':pizzas })
+
     return render(request, 'menu-menu/menu.html', context={
         'pizzas': Pizza.objects.all().order_by('name')
     })
---
+++ static/js/pizzas.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#search-btn').on('click',function(e){
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        $.ajax({
+            url: '/pizzas?search_filter='+ searchText,
+            type: 'GET',
+            success:function(resp){
+                var newHtml = resp.data.map(d => {
+                    return `<div class="well pizza">
+                                <a href="/pizzas/${d.id}">
+                                    <img class="pizza-img" src="${d.image}"/>
+                                    <h4>$(d.name)</h4>
+                                    <p>${d.toppings}</p>
+                                </a>
+                            </div`
+                });
+                $('.pizzas').html(newHtml.join(''));
+                $('#search-box').val('');
+            },
+            error:function(xhr,status,error){
+                console.log(error);
+            }
+        })
+    });
+});
