d4ac0a7eb945bd4df2dba8fc343809c8531ad24b
dorajohanns <89972552+dorajohanns@users.noreply.github.com>
2023-05-08 18:17:08 +0000

    bæta við virkni í körfu

--- static/js/basket.js
+++ static/js/basket.js
@@ -4,25 +4,33 @@ const addToBasket = (list) => {
     openNav();
 }
 
+const removeFromBasket = (id) => {
+    sessionStorage.removeItem(id);
+    updateBasket();
+}
+
 const updateBasket = () => {
     const cart = document.getElementById("cart-pizzas");
     cart.innerHTML = "";
     Object.keys(sessionStorage).map((key) => {
         let pizzaArray = sessionStorage.getItem(key).split(",");
         let pizzadiv = document.createElement("div");
-        let nametag = document.createElement("h2");
+        let nametag = document.createElement("h1");
         nametag.textContent = pizzaArray[1].replaceAll('"', '');
         let imgtag = document.createElement("img");
         imgtag.alt = pizzaArray[1];
         imgtag.src = "../static/images/" + pizzaArray[2].replaceAll('"', '');
-        let priceTag = document.createElement("p");
-        priceTag.textContent = pizzaArray[3].replace(']', '');
+        let priceTag = document.createElement("h3");
+        priceTag.textContent = pizzaArray[3].replace(']', '') + " Kr";
         let delbtn = document.createElement("button");
+        delbtn.onclick = () => removeFromBasket(key);
         delbtn.textContent = "remove item from cart";
+        delbtn.id = "delbtn";
         pizzadiv.appendChild(nametag);
         pizzadiv.appendChild(imgtag);
         pizzadiv.appendChild(priceTag);
         pizzadiv.appendChild(delbtn);
+        pizzadiv.id = "pizza-container"
         cart.appendChild(pizzadiv);
     })
 }
@@ -35,3 +43,4 @@ function openNav() {
 function closeNav() {
   document.getElementById("cartNav").style.width = "0%";
 }
+
