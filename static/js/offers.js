cd05da7869dc6129033fe56dac67fc45d5b8285f
danekpr1 <112620766+danekpr1@users.noreply.github.com>
2023-05-10 11:20:14 +0000

    offers search virkni og röðun bla bla

--- static/js/offers.js
+++ static/js/offers.js
@@ -1,16 +1,21 @@
-$(document).ready(function (){
+$(document).ready(function(){
     var sortOrder = 'asc';
     var sortByPriceOrder = 'asc';
 
-    $('#offers-search-btn').on('click',function (e){
+    $('#offers-search-btn').on('click', function(e) {
         e.preventDefault();
         var searchText = $('#offers-search-box').val();
         $.ajax({
-            url:'/offers?search_filter=' + searchText,
+            url: '/offers?search_filter=' + searchText,
             type: 'GET',
-            success:function (resp){
+            success: function(resp) {
                 var data = resp.data;
-
+                // Filter by name
+                if (searchText) {
+                    data = data.filter(function(item) {
+                        return item.name.toLowerCase().includes(searchText.toLowerCase());
+                    });
+                }
                 // Sort by name
                 data.sort(function(a, b) {
                     var nameA = a.name.toUpperCase();
@@ -30,38 +35,36 @@ $(document).ready(function (){
                             return -1;
                         }
                     }
-                    // names must be equal
                     return 0;
                 });
-
                 // Sort by price
-                data.sort(function(a, b) {
-                    var priceA = a.price;
-                    var priceB = b.price;
-                    if (sortByPriceOrder === 'asc') {
-                        return priceA - priceB;
-                    } else {
-                        return priceB - priceA;
-                    }
-                });
-
-                var newHtml = data.map(d => {
-                    return `<div class="well offer-div">
+                if (sortByPriceOrder === 'asc') {
+                    data.sort(function(a, b) {
+                        return a.price - b.price;
+                    });
+                } else {
+                    data.sort(function(a, b) {
+                        return b.price - a.price;
+                    });
+                }
+              //  <img className="menu-img" src="../../static/images/${d.image}"/>
+                var newHtml = data.map(function(d) {
+                    return `<div class="menu-div">
                                 <a href="/offers/${d.id}">
-                                    <img class="offer-img" src="../../static/images/${d.image}"/>
-                                    <h4 class="offer-name">${d.name}</h4>
-                                    <p class="offer-description">${d.description}</p>
-                                    <span class="offer-price">${d.price}</span>
+                              
+                                    <h3 class="pizza-name">${d.name}</h3>
+                                    <p class="pizza-top">${d.desc}</p>
+                                    <span class="price">${d.price}</span>
                                 </a>
-                            </div>`
+                            </div>`;
                 });
                 $('.offers').html(newHtml.join(''));
                 $('#offers-search-box').val('');
             },
-            error:function(xhr,status,error){
+            error: function(xhr, status, error) {
                 console.error(error);
             }
-        })
+        });
     });
 
     $('#offers-reset-btn').on('click', function(e) {
@@ -69,7 +72,6 @@ $(document).ready(function (){
         location.reload();
     });
 
-    // add event listener for sort button by name
     $('#offers-sort-btn').on('click', function(e) {
         e.preventDefault();
         if (sortOrder === 'asc') {
@@ -82,7 +84,6 @@ $(document).ready(function (){
         $('#offers-search-btn').click();
     });
 
-    // add event listener for sort button by price
     $('#offers-sort-by-price-btn').on('click', function(e) {
         e.preventDefault();
         if (sortByPriceOrder === 'asc') {
@@ -95,4 +96,3 @@ $(document).ready(function (){
         $('#offers-search-btn').click();
     });
 });
-
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from user.forms.profile_form import ProfileForm
+from user.models import Profile
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
@@ -11,4 +12,20 @@ def register(request):
     return render(request,'user/register.html',{
         'form': UserCreationForm()
     })
+
+
+def profile(request):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method=='POST':
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    return render(request, 'user/profile.html',{
+        'form':ProfileForm(instance=profile)
+    })
+
+
 
