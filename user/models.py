e2cac825cc9385ca3e802afd1f69be05a62cd87a
danekpr1 <112620766+danekpr1@users.noreply.github.com>
2023-05-11 14:45:19 +0000

    sort by tyytypee

--- user/models.py
+++ user/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=999)
     profile_image = models.CharField(max_length=999)
