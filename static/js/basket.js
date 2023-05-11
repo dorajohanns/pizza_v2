749ed4ee9dfe9a199432fc718323b7fde9990dc0
dorajohanns <89972552+dorajohanns@users.noreply.github.com>
2023-05-11 10:26:30 +0000

    add offers to basket more

--- static/js/basket.js
+++ static/js/basket.js
@@ -22,62 +22,75 @@ const updateBasket = () => {
     let priceSum = 0;
 
     Object.keys(sessionStorage).map((key) => {
-        let pizzaArray = JSON.parse(sessionStorage.getItem(key))
-        console.log(pizzaArray);
+        if (key != "offers") {
+            let pizzaArray = JSON.parse(sessionStorage.getItem(key))
 
-        let pizzadiv = document.createElement("div");
+            let pizzadiv = document.createElement("div");
 
-        let nametag = document.createElement("h1");
-        nametag.textContent = pizzaArray[1]
+            let nametag = document.createElement("h1");
+            nametag.textContent = pizzaArray[1]
 
-        let imgtag = document.createElement("img");
-        imgtag.alt = pizzaArray[1];
-        imgtag.src = "../static/images/" + pizzaArray[2]
+            let imgtag = document.createElement("img");
+            imgtag.alt = pizzaArray[1];
+            imgtag.src = "../static/images/" + pizzaArray[2]
 
-        let priceTag = document.createElement("h3");
-        priceTag.textContent = pizzaArray[3] + " Kr";
+            let priceTag = document.createElement("h3");
+            priceTag.textContent = pizzaArray[3] + " Kr";
 
-        let quantityCounter = document.createElement("div");
-        quantityCounter.id = "quantity-counter";
-        let minus = document.createElement("button");
-        minus.textContent = "-";
-        minus.onclick = () => decreaseQuantity(pizzaArray[0]);
-        minus.className = "quantity-btn"
-        let quantity = document.createElement("span");
-        quantity.textContent = pizzaArray[4];
-        quantity.className = "quantity"
-        let plus = document.createElement("button");
-        plus.textContent = "+";
-        plus.onclick = () => increaseQuantity(pizzaArray[0]);
-        plus.className = "quantity-btn"
-        quantityCounter.appendChild(minus);
-        quantityCounter.appendChild(quantity);
-        quantityCounter.appendChild(plus);
+            let quantityCounter = document.createElement("div");
+            quantityCounter.id = "quantity-counter";
+            let minus = document.createElement("button");
+            minus.textContent = "-";
+            minus.onclick = () => decreaseQuantity(pizzaArray[0]);
+            minus.className = "quantity-btn"
+            let quantity = document.createElement("span");
+            quantity.textContent = pizzaArray[4];
+            quantity.className = "quantity"
+            let plus = document.createElement("button");
+            plus.textContent = "+";
+            plus.onclick = () => increaseQuantity(pizzaArray[0]);
+            plus.className = "quantity-btn"
+            quantityCounter.appendChild(minus);
+            quantityCounter.appendChild(quantity);
+            quantityCounter.appendChild(plus);
 
 
-        let delbtn = document.createElement("button");
-        delbtn.onclick = () => removeFromBasket(key);
-        delbtn.textContent = "remove item from cart";
-        delbtn.id = "delbtn";
+            let delbtn = document.createElement("button");
+            delbtn.onclick = () => removeFromBasket(key);
+            delbtn.textContent = "remove item from cart";
+            delbtn.id = "delbtn";
 
-        pizzadiv.appendChild(nametag);
-        pizzadiv.appendChild(imgtag);
-        pizzadiv.appendChild(priceTag);
-        pizzadiv.appendChild(quantityCounter);
-        pizzadiv.appendChild(delbtn);
+            pizzadiv.appendChild(nametag);
+            pizzadiv.appendChild(imgtag);
+            pizzadiv.appendChild(priceTag);
+            pizzadiv.appendChild(quantityCounter);
+            pizzadiv.appendChild(delbtn);
 
-        pizzadiv.id = "pizza-container"
-        cart.appendChild(pizzadiv);
-        priceSum += (pizzaArray[3] * pizzaArray[4]);
+            pizzadiv.id = "pizza-container"
+            cart.appendChild(pizzadiv);
+            priceSum += (pizzaArray[3] * pizzaArray[4]);
+        }
     });
 
+    priceSum += offersTotalPrice();
     const priceTag = document.getElementById("priceSum");
     priceTag.textContent = "Total: " + priceSum + " Kr";
+
+    updateBasketOffers();
     basketStatus();
+}
+
+const offersTotalPrice = () => {
+    let sum = 0
+    JSON.parse(sessionStorage.getItem("offers")).map((offer) => {
+        sum += offer[3] * offer[4]
+    })
+    return sum
 }
 
 const clearCart = () => {
     sessionStorage.clear();
+    createOfferStorage();
     updateBasket();
 }
 
@@ -100,7 +113,7 @@ const increaseQuantity = (id) => {
 }
 
 const basketStatus = () => {
-    if (Object.keys(sessionStorage).length === 0) {
+    if (Object.keys(sessionStorage).length === 1 && JSON.parse(sessionStorage.getItem("offers")).length === 0) {
         document.getElementById("empty-cart").style.display = "block";
         document.getElementById("clear-cart").style.display = "none";
         document.getElementById("non-empty-cart").style.display = "none";
@@ -125,4 +138,12 @@ function closeNav() {
   document.getElementById("cartNav").style.width = "0%";
 }
 
-basketStatus()
+const createOfferStorage = () => {
+    sessionStorage.setItem("offers", JSON.stringify([]));
+}
+
+basketStatus();
+
+if (!sessionStorage.getItem("offers")) {
+    createOfferStorage();
+}
--- static/js/offersPicker.js
+++ static/js/offersPicker.js
@@ -1,15 +1,13 @@
 
 let selected = [];
 
-
-const addOfferToBasket = (name) => {
+const selectPizza = (name) => {
     if (selected.length < 2 && selected.includes(name) == false) {
         selected.push(name);
     } else if (selected.includes(name) == true) {
-        const removeArray = selected.filter(function (name1) {
+        selected = selected.filter(function (name1) {
             return name1 !== name;
         });
-        selected = removeArray;
         uncolorPizzaContainer(name);
     }
     updatePizzas();
@@ -23,13 +21,8 @@ const updatePizzas = () => {
 }
 
 const buttonUpdate = () => {
-    if (selected.length === 2) {
-        document.getElementById("offer-add").disabled = false;
-    } else {
-        document.getElementById("offer-add").disabled = true;
-    }
+    document.getElementById("offer-add").disabled = selected.length !== 2;
 }
-
 
 const colorPizzaContainer = (name) => {
     const selectedPizza = document.getElementById(String(name));
@@ -39,4 +32,108 @@ const colorPizzaContainer = (name) => {
 const uncolorPizzaContainer = (name) => {
     const selectedPizza = document.getElementById(String(name));
     selectedPizza.style.backgroundColor = "white";
+}
+
+const addToBasketOffer = (list) => {
+    const offerList = JSON.parse(sessionStorage.getItem("offers"));
+    const offerExists = sameOfferExists();
+    if (!offerExists) {
+        offerList.push([list[0], list[1], selected, list[2], 1]);
+        sessionStorage.setItem("offers", JSON.stringify(offerList));
+        updateBasketOffers();
+        updateBasket();
+    }
+    openNav();
+}
+
+const updateBasketOffers = () => {
+    const offerList = JSON.parse(sessionStorage.getItem("offers"));
+    const offerContainer = document.getElementById("cart-offers");
+    offerContainer.innerHTML = "";
+    offerList.map((offer) => {
+        let offerdiv = document.createElement("div");
+
+        let nametag = document.createElement("h1");
+        nametag.textContent = offer[0];
+
+        let imgtag = document.createElement("img");
+        imgtag.alt = offer[0];
+        imgtag.src = "../static/images/" + offer[1];
+
+        let includedPizzas = document.createElement("span")
+        offer[2].map((pizza) => {
+            includedPizzas.textContent += pizza;
+        })
+
+        let priceTag = document.createElement("h3");
+        priceTag.textContent = offer[3] + " Kr";
+
+        let quantityCounter = document.createElement("div");
+        quantityCounter.id = "quantity-counter";
+        let minus = document.createElement("button");
+        minus.textContent = "-";
+        minus.onclick = () => removeOffer(selected, false);
+        minus.className = "quantity-btn"
+        let quantity = document.createElement("span");
+        quantity.textContent = offer[4];
+        quantity.className = "quantity"
+        let plus = document.createElement("button");
+        plus.textContent = "+";
+        plus.onclick = () => increaseOffer(selected);
+        plus.className = "quantity-btn"
+        quantityCounter.appendChild(minus);
+        quantityCounter.appendChild(quantity);
+        quantityCounter.appendChild(plus);
+
+        let delbtn = document.createElement("button");
+        delbtn.onclick = () => removeOffer(selected, true);
+        delbtn.textContent = "remove item from cart";
+        delbtn.id = "delbtn";
+
+        offerdiv.appendChild(nametag);
+        offerdiv.appendChild(imgtag);
+        offerdiv.appendChild(includedPizzas);
+        offerdiv.appendChild(priceTag);
+        offerdiv.appendChild(quantityCounter);
+        offerdiv.appendChild(delbtn);
+
+        offerdiv.id = "pizza-container";
+        offerContainer.appendChild(offerdiv);
+    })
+}
+
+const removeOffer = (pizzas, removeAll) => {
+    const offers = JSON.parse(sessionStorage.getItem("offers"));
+    offers.map((offer, index) => {
+        if (offer[2][0] == pizzas[0] && offer[2][1] == pizzas[1]) {
+            if (offer[4] == 1 || removeAll) {
+                offers.splice(index, 1);
+            } else {
+                offer[4] -= 1;
+            }
+        }
+    });
+    sessionStorage.setItem("offers", JSON.stringify(offers));
+    updateBasket();
+    updateBasketOffers();
+}
+
+const increaseOffer = (pizzas) => {
+    selected = pizzas
+    sameOfferExists();
+    updateBasketOffers();
+    updateBasket();
+}
+
+const sameOfferExists = () => {
+    let exists = false
+    let storage = JSON.parse(sessionStorage.getItem("offers"))
+    storage.map((offer) => {
+        if (offer[2][0] == selected[0] && offer[2][1] == selected[1]) {
+            exists = true;
+            offer[4] += 1;
+            sessionStorage.setItem("offers", JSON.stringify(storage));
+        }
+    });
+    return exists;
 }
